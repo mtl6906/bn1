a38e33faedcca36d27af64d690fa86f9cb3e0110
mtl6906 <zzz5486129@gmail.com>
2021-09-18 15:44:26 +0800

    add buy

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "ls/http/Request.h"
+#include "ls/http/QueryString.h"
 #include "ls/io/InputStream.h"
 #include "ls/io/OutputStream.h"
 #include "ls/ssl/Client.h"
 #include "ls/net/Client.h"
+#include "ls/json/API.h"
+#include "ls/SHA256.h"
 #include "string"
 #include "vector"
 #include "iostream"
@@ -11,18 +14,19 @@
 using namespace ls;
 using namespace std;
 
-char *ip, *url;
+char *ip, *url, *secretKey, *apiKey;
 
-string transacation(const string &method, const string &url, const string &body = "")
+string transacation(const string &method, const string &url, const string &body = "", const map<string, string> &attributes = map<string, string>())
 {
 	http::Request request;
 	request.setDefaultHeader();
-	request.getMethod() = "GET";
-	request.getURL() = "/api/v3/ticker/bookTicker?symbol=FTMUSDT";
+	request.getMethod() = method;
+	request.getURL() = url;
 	request.getVersion() = "HTTP/1.1";
 	request.setAttribute("Host", "api.binance.com");
 	request.setAttribute("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0");
-
+	for(auto &attribute : attributes)
+		request.setAttribute(attribute.first, attribute.second);
 	ssl::Client sslClient;
 	unique_ptr<ssl::Connection> connection(sslClient.getConnection(net::Client(ip, 443).connect()));
 	connection -> setHostname(url);
@@ -37,11 +41,44 @@ string transacation(const string &method, const string &url, const string &body 
 	return in.split();
 }
 
-void get
+vector<double> getPrice()
+{
+	vector<double> prices(1);
+	string text = transacation("GET", "/api/v3/ticker/bookTicker?symbol=GALAUSDT");
+	cout << text << endl;
+	auto root = json::api.decode(text);
+	json::api.get(root, "bidPrice", prices[0]);
+	json::api.get(root, "askPrice", prices[1]);
+	return prices;
+}
+
+string buy(const string &coin, double price, int number)
+{
+	map<string, string> attribute;
+	attribute["X-MBX-APIKEY"] = apiKey;
+	string url = "/api/v2/order";
+	http::QueryString qs;
+	qs.setParameter("symbol", coin);
+	qs.setParameter("side", "BUY");
+	qs.setParameter("type", "LIMIT");
+	qs.setParameter("timeForce", "GTC");
+	qs.setParameter("quantity", to_string(number));
+	qs.setParameter("price", to_string(price));
+	qs.setParameter("timestamp", to_string(time(NULL)));
+	string body = qs.toString();
+	ls::SHA256 sha256;
+	string signature = sha256.hmac(body, secretKey);
+	body.append("&sinature=");
+	body.append(signature);
+	return transacation("POST", "/api/v2/order", body, attribute);
+}
 
 int main(int argc, char **argv)
 {
 	ip = argv[1];
-	url = argv[1];
-	cout << getPrice() << endl;
+	url = argv[2];
+	apiKey = argv[3];
+	secretKey = argv[4];
+	getPrice();
+	cout << buy("GALAUSDT", 0.08, 200) << endl;
 }
